e59e48b265033d056827132e3427cf2693a2356d
Nathnael <nathnaelds@gmail.com>
2019-05-24 22:40:37 +0300

    Create a socket fd and set server address

--- ClientSocket.cpp
+++ ClientSocket.cpp
@@ -11,5 +11,40 @@ class ClientSocket {
         struct sockaddr_in serverAddress;
     
     
-     
+    public:
+        ClientSocket(int method) {
+            // Choose whether to use Stream or datagram
+            transferMethod = method;
+
+            // Add an if function to differentiate between
+            /* Code */
+
+
+
+            // Set up a socket file descriptor
+            if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+            {
+                perror("failed to get socket descriptor");
+                exit(1);
+            }
+        }
+
+        // Connect to a server given an IP and Port number
+        void connectTo(char* ipAddress, int portNumber){
+            ip = ipAddress;
+            portNo = portNumber;
+            
+            // Get the binary form of the IP Address
+            if((host = gethostbyname(ip)) == NULL)
+            {
+                perror("failed to resolve host");
+                exit(1);
+            }
+
+            // setting up server address
+            serverAddress.sin_family = AF_INET;
+            serverAddress.sin_port = htons(portNo);
+            serverAddress.sin_addr.s_addr = inet_addr(
+                        inet_ntoa(*((struct in_addr *)host->h_addr)));
+        }
 }
