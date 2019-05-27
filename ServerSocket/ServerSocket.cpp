ad18c2ac96e23e33f509547f995f5a651725769b
Nathnael <nathnaelds@gmail.com>
2019-05-27 12:06:30 +0300

    Added a function to start the server

--- ServerSocket/ServerSocket.cpp
+++ ServerSocket/ServerSocket.cpp
@@ -26,3 +26,41 @@ void ServerSocket::bindAndListen(int portNumber, int connectionLimit)
         exit(1);
     }
 }
+
+void ServerSocket::startServer()
+{
+    while (1)
+    {
+        char buffer[1024];
+        if (recvfrom(sockfd, buffer, sizeof(buffer), 0, (struct sockaddr *)&clientAddress, (socklen_t *)&addrlen) < 0)
+        {
+            perror("recvfrom error");
+            exit(1);
+        }
+        else
+        {
+            //request successfully accepted
+            pid = fork();
+            if (pid == 0)
+            {
+                //Handl the request on the child process
+                buf = (char *)malloc(1024);
+                snprintf(buf, len, "%ld: %s\n", s_ack, buffer);
+                n_bytes = sendto(sockfd, buf, 1024, 0, (struct sockaddr *)&clientAddress, sizeof(clientAddress));
+                if (n_bytes < 0)
+                {
+                    perror("can not send data");
+                    exit(1);
+                }
+                else
+                {
+                    s_ack++;
+                }
+            }
+            else
+            {
+                wait(NULL);
+            }
+        }
+    }
+}
